9e89d9fd6a919db7c48fec58ee275f97232e397d
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-07-09 21:49:51 +0530

    refactor: verify key response change
    The `VerifyKey` function now returns `false, err` if an error
    occurs. As per initial thought it returned a pointer to bool but
    after having a dicussion on Discord it was decided to change it.
    
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- features/key_verify.go
+++ features/key_verify.go
@@ -15,10 +15,10 @@ type VerifyRequestSchema struct {
 }
 
 type VerifyResponseSchema struct {
-	Valid *bool `json:"valid"`
+	Valid bool `json:"valid"`
 }
 
-func VerifyRequest(key string) (*bool, error) {
+func VerifyRequest(key string) (bool, error) {
 	
 	verifyReq := VerifyRequestSchema{
 		Key: key,
@@ -26,13 +26,13 @@ func VerifyRequest(key string) (*bool, error) {
 	payload, err := json.Marshal(verifyReq)
 	if err != nil {
 		fmt.Println("Error marshaling request:", err)
-		return nil, err
+		return false, err
 	}
 
 	req, err := http.NewRequest("POST", utils.UNKEY_API_URL+"/keys/verify", bytes.NewBuffer(payload))
 	if err != nil {
 		fmt.Println("Error creating request:", err)
-		return nil, err
+		return false, err
 	}
 	req.Header.Set("Content-Type", "application/json")
 
@@ -40,21 +40,21 @@ func VerifyRequest(key string) (*bool, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error sending request:", err)
-		return nil, err
+		return false, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
-		return nil, err
+		return false, err
 	}
 
 	var verifyResp VerifyResponseSchema
 	err = json.Unmarshal(body, &verifyResp)
 	if err != nil {
 		fmt.Println("Error parsing JSON response:", err)
-		return nil, err
+		return false, err
 	}
 
 	return verifyResp.Valid, nil
