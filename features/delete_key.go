d0b2a1dd9f2c4bf63c4f352a2d00bb55e49117a3
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-07-13 00:55:41 +0530

    feat: DeleteKey function
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

---
+++ features/delete_key.go
@@ -0,0 +1,40 @@
+package features
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/WilfredAlmeida/unkey-go/utils"
+)
+
+func DeleteKey(keyID string, authToken string) (bool, error) {
+	// Create the HTTP request
+	req, err := http.NewRequest("DELETE", utils.UNKEY_API_URL+"/keys/"+keyID, nil)
+	if err != nil {
+		return false, err
+	}
+
+	// Set the request headers
+	req.Header.Set("Authorization", "Bearer "+authToken)
+
+	// Send the HTTP request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	// Check the response status code
+	if resp.StatusCode == http.StatusOK {
+		return true, nil
+	} else {
+		return false, fmt.Errorf(string(body))
+	}
+}
--- main.go
+++ main.go
@@ -10,31 +10,40 @@ func main() {
 	// var isKeyValid, _ = features.VerifyRequest("key_3ZZ7faUrkfv1YAhffAcnKW74")
 	// fmt.Println("Is key valid:", isKeyValid)
 
-	key := features.CreateKeyRequest{
-		APIId:      "api_ThrZA83W5BeUE3xm8onQ45",
-		Prefix:     "xyz",
-		ByteLength: 16,
-		OwnerId:    "wilfred",
-		Meta: features.Meta{
-			"hello": "world",
-		},
-		Expires: 1696951966471,
-		RateLimit: features.CreateKeyRateLimit{
-			Type:           "fast",
-			Limit:          10,
-			RefillRate:     1,
-			RefillInterval: 1000,
-		},
-	}
+	// key := features.CreateKeyRequest{
+	// 	APIId:      "api_ThrZA83W5BeUE3xm8onQ45",
+	// 	Prefix:     "xyz",
+	// 	ByteLength: 16,
+	// 	OwnerId:    "wilfred",
+	// 	Meta: features.Meta{
+	// 		"hello": "world",
+	// 	},
+	// 	Expires: 1696951966471,
+	// 	RateLimit: features.CreateKeyRateLimit{
+	// 		Type:           "fast",
+	// 		Limit:          10,
+	// 		RefillRate:     1,
+	// 		RefillInterval: 1000,
+	// 	},
+	// }
 
 	authToken := "unkey_3ZeRMoXP9NFgsSxjnNtjSGLF"
 
-	response, err := features.CreateKey(key, authToken)
+	// response, err := features.CreateKey(key, authToken)
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	return
+	// }
+
+	// fmt.Println("Key: ", response.Key)
+	// fmt.Println("Key ID: ", response.KeyId)
+
+	response, err := features.DeleteKey("key_VNZez78oQTwyB6ggSaGtTF", authToken)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	fmt.Println("Key: ", response.Key)
-	fmt.Println("Key ID: ", response.KeyId)
+	fmt.Println("Response: ", response)
+
 }
